86faee2ad082e3248202c48d5ee90506f0b4aabc
Yuzan <yuzan.ymmt@gmail.com>
2025-06-03 20:17:51 +0900

    課題1-3を追加

--- start-folder/JavaScriptWork/work_1_3/1_3.js
+++ start-folder/JavaScriptWork/work_1_3/1_3.js
@@ -1,11 +1,11 @@
-const getTotalPrice = (price,quantity) => price * quantity;
+const getTotalPrice = (price,quantity) => price*quantity;
 
 const addTax = total => total * 1.1;
 
-const total = getTotalPrice(1000, 2); // → 2000
+const total = getTotalPrice(1000,2);
 
-console.log(`税抜金額は${total}円です`);
+console.log(`税抜金額は${total}円です`)
 
-const taxedTotal = addTax(total);     // → 2200
+const taxedTotal = addTax(total);
 
-console.log(`税込金額は${taxedTotal}円です`);
+console.log(`税込金額は${taxedTotal}円です`)
